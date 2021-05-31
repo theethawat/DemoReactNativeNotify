4b843fa21c2f61816c24409018e819196b5dba83
Theethawat Savastham <5910110150@psu.ac.th>
2021-06-01 00:04:43 +0700

    เอาการ Handle ของ Messaging มาใส่ไว้

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import messaging from "@react-native-firebase/messaging";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      console.log("New FCM message is Arrive", remoteMessage);
+    });
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      console.log(
+        "Notification cause app wake from background state",
+        remoteMessage.notification
+      );
+    });
+
+    // Check Initial Notification is Avaliable
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            "Notification Cause app to open from quit state",
+            remoteMessage.notification
+          );
+        }
+      });
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { registerRootComponent } from "expo";
-
+import messaging from "@react-native-firebase/messaging";
 import App from "./App";
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log("Message handle in Background state", remoteMessage);
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
+
 registerRootComponent(App);
